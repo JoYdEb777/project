6dce53629b8481a435442f10e0c246eafb2aa056
JoYdEb777 <joydebroy8325@gmail.com>
2026-03-01 01:07:30 +0530

    midlleware

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const postRouter = require("./routes/post.routes")
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
+
 app.use("/api/auth", authRouter)
 app.use("/api/posts", postRouter)
+
 module.exports = app
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -3,9 +3,13 @@ const postRouter = express.Router()
 const PostController = require("../controllers/post.controller")
 const multer = require("multer")
 const upload = multer({storage:multer.memoryStorage()})
+const identifyUser = require("../middlewares/auth.middleware")
 
+postRouter.post("/",upload.single("image"),identifyUser, PostController.createPostController )
 
-postRouter.post("/",upload.single("image"), PostController.createPostController )
+postRouter.get("/",identifyUser, PostController.getPostController)
+
+postRouter.get("/details/:postId",identifyUser, PostController.getPostDetailsController)
 
 
 module.exports = postRouter
